ce289cbab0790a585a49e6bcec2aeead6d68b5cd
Paul Welden <paul.j.welden@gmail.com>
2015-12-29 00:12:41 -0600

    gulp tweaks

--- Calculator/gulpfile.js
+++ Calculator/gulpfile.js
@@ -7,20 +7,15 @@ var nunit = require('gulp-nunit-runner');
 var exec = require('gulp-exec');
 var gulpSequence = require('gulp-sequence');
 
+gulp.task('updateAssemblyInfo', function () {
 
-var params = {
-	buildVersion: args.buildVersion || '1.0.0.0',
-	nunitRunner: args.nunitRunner || __dirname + '\\packages\\NUnit.Console.3.0.1\\tools\\nunit3-console.exe',
-	nugetPath: args.nugetPath || process.env.nugetPath || 'nuget'
-}
+	var buildVersion = args.buildVersion || process.env.buildVersion || '1.0.0.0';
 
-
-gulp.task('assemblyInfo', function() {
 	return gulp
         .src('**/AssemblyInfo.cs')
         .pipe(assemblyInfo({
-        	version: params.buildVersion,
-        	fileVersion: params.buildVersion,
+        	version: buildVersion,
+        	fileVersion: buildVersion,
         }))
         .pipe(gulp.dest('.'));
 });
@@ -36,8 +31,8 @@ gulp.task('clean', function () {
         }));
 });
 
-
 gulp.task('build', function () {
+
 	return gulp
         .src('**/*.sln')
         .pipe(msbuild({
@@ -49,34 +44,32 @@ gulp.task('build', function () {
 });
 
 gulp.task('test', function () {
+
 	return gulp
         .src(['**/bin/**/*Tests.dll'], { read: false })
         .pipe(nunit({
         	teamcity: true,
-        	executable: params.nunitRunner
+        	executable: args.nunitRunner || process.env.nunitRunner || __dirname + '\\packages\\NUnit.Console.3.0.1\\tools\\nunit3-console.exe'
         }));
 });
 
-gulp.task('nugetRestore',function() {
+gulp.task('nugetRestore', function () {
+
 	var options = {
 		continueOnError: false, // default = false, true means don't emit error event 
 		pipeStdout: false, // default = false, true means stdout is written to file.contents 
-		nugetPath: params.nugetPath
+		nugetPath: args.nugetPath || process.env.nugetPath || 'nuget'
 	};
+
 	var reportOptions = {
 		err: true, // default = true, false means don't write err 
 		stderr: true, // default = true, false means don't write stderr 
 		stdout: true // default = true, false means don't write stdout 
-	}
+	};
 
 	return gulp.src('**/*.sln')
 	  .pipe(exec('<%= options.nugetPath %> restore <%= file.path %>', options))
 	  .pipe(exec.reporter(reportOptions));
- 
 });
 
-gulp.task('default', function () {
-	console.log(params.buildVersion);
-});
-
-gulp.task('ci', gulpSequence('clean', 'nugetRestore', 'build', 'test') );
+gulp.task('ci', gulpSequence('clean', 'nugetRestore', 'build', 'test'));
